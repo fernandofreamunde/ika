37fe847d3c1b8c24a54bbe44d51af7d1523f6a8c
Fernando Andrade <fernandofreamunde@gmail.com>
2025-04-21 11:45:52 +0200

    add a simple helper

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -11,6 +11,10 @@ func (s *Server) RegisterRoutes() http.Handler {
 
 	// Register routes
 	mux.HandleFunc("/", s.HelloWorldHandler)
+	mux.HandleFunc("/register", s.RegisterUserHandler)
+	mux.HandleFunc("/login", s.LoginHandler)
+	mux.HandleFunc("/new_message", s.NewMessageHandler)
+	mux.HandleFunc("/chatrooms", s.NewChatRoomHandler)
 
 	mux.HandleFunc("/health", s.healthHandler)
 
@@ -39,15 +43,27 @@ func (s *Server) corsMiddleware(next http.Handler) http.Handler {
 
 func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	resp := map[string]string{"message": "Hello World"}
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	if _, err := w.Write(jsonResp); err != nil {
-		log.Printf("Failed to write response: %v", err)
-	}
+	respondWithJson(resp, 200, w)
+}
+
+func (s *Server) RegisterUserHandler(w http.ResponseWriter, r *http.Request) {
+	resp := map[string]string{"message": "Register User!"}
+	respondWithJson(resp, 200, w)
+}
+
+func (s *Server) LoginHandler(w http.ResponseWriter, r *http.Request) {
+	resp := map[string]string{"message": "Login User!"}
+	respondWithJson(resp, 200, w)
+}
+
+func (s *Server) NewMessageHandler(w http.ResponseWriter, r *http.Request) {
+	resp := map[string]string{"message": "New Message!"}
+	respondWithJson(resp, 200, w)
+}
+
+func (s *Server) NewChatRoomHandler(w http.ResponseWriter, r *http.Request) {
+	resp := map[string]string{"message": "Register Chat Rooms!"}
+	respondWithJson(resp, 200, w)
 }
 
 func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
@@ -58,6 +74,22 @@ func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	if _, err := w.Write(resp); err != nil {
+		log.Printf("Failed to write response: %v", err)
+	}
+}
+
+func respondWithJson(payload interface{}, statusCode int, w http.ResponseWriter) {
+
+	jsonResp, err := json.Marshal(payload)
+	if err != nil {
+		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(statusCode)
+	w.Header().Set("Content-Type", "application/json")
+
+	if _, err := w.Write(jsonResp); err != nil {
 		log.Printf("Failed to write response: %v", err)
 	}
 }
