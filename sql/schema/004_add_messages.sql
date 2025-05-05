1f8d982d2a1848968cfc1d0c23c14043faf3d3dd
Fernando Andrade <fernandofreamunde@gmail.com>
2025-05-05 17:25:41 +0200

    woops make messages non unique

--- sql/schema/004_add_messages.sql
+++ sql/schema/004_add_messages.sql
@@ -6,7 +6,7 @@ CREATE TABLE messages(
 	author_id UUID,
 	chatroom_id UUID,
 	type VARCHAR(256) NOT NULL,
-	content TEXT UNIQUE DEFAULT NULL,
+	content TEXT DEFAULT NULL,
 	CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE,
 	CONSTRAINT fk_chatroom_id FOREIGN KEY (chatroom_id) REFERENCES chatrooms(id) ON DELETE CASCADE
 );
